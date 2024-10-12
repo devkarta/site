2f951062553062d59e51aad2e20ebbc4dfe3919a
Ilham Ghifari <ilham@manyvisual.com>
2024-10-13 01:49:39 +0700

    first version

--- app/writing/[slug]/page.tsx
+++ app/writing/[slug]/page.tsx
@@ -1,61 +1,63 @@
 import { notFound } from 'next/navigation'
-import { CustomMDX } from 'app/components/mdx'
-import { formatDate, getBlogPosts } from 'app/blog/utils'
-import { baseUrl } from 'app/sitemap'
+import { formatDate, getWritingPosts } from '../utils';
+import { CustomMDX } from '@/components/mdx';
+
+
+// import { baseUrl } from 'app/sitemap'
 
 export async function generateStaticParams() {
-  let posts = getBlogPosts()
+  const posts = getWritingPosts();
 
   return posts.map((post) => ({
     slug: post.slug,
   }))
 }
 
-export function generateMetadata({ params }) {
-  let post = getBlogPosts().find((post) => post.slug === params.slug)
+// export function generateMetadata({ params }) {
+//   const post = getBlogPosts().find((post) => post.slug === params.slug)
+//   if (!post) {
+//     return
+//   }
+
+//   const {
+//     title,
+//     publishedAt: publishedTime,
+//     summary: description,
+//     image,
+//   } = post.metadata
+//   const ogImage = image
+//     ? image
+//     : `${baseUrl}/og?title=${encodeURIComponent(title)}`
+
+//   return {
+//     title,
+//     description,
+//     openGraph: {
+//       title,
+//       description,
+//       type: 'article',
+//       publishedTime,
+//       url: `${baseUrl}/blog/${post.slug}`,
+//       images: [
+//         {
+//           url: ogImage,
+//         },
+//       ],
+//     },
+//     twitter: {
+//       card: 'summary_large_image',
+//       title,
+//       description,
+//       images: [ogImage],
+//     },
+//   }
+// }
+
+export default function Writing({ params }: { params: { slug: string } }) {
+  const post = getWritingPosts().find((post) => post.slug === params.slug);
+
   if (!post) {
-    return
-  }
-
-  let {
-    title,
-    publishedAt: publishedTime,
-    summary: description,
-    image,
-  } = post.metadata
-  let ogImage = image
-    ? image
-    : `${baseUrl}/og?title=${encodeURIComponent(title)}`
-
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      type: 'article',
-      publishedTime,
-      url: `${baseUrl}/blog/${post.slug}`,
-      images: [
-        {
-          url: ogImage,
-        },
-      ],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title,
-      description,
-      images: [ogImage],
-    },
-  }
-}
-
-export default function Blog({ params }) {
-  let post = getBlogPosts().find((post) => post.slug === params.slug)
-
-  if (!post) {
-    notFound()
+    notFound();
   }
 
   return (
@@ -65,19 +67,19 @@ export default function Blog({ params }) {
         suppressHydrationWarning
         dangerouslySetInnerHTML={{
           __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'BlogPosting',
+            "@context": "https://schema.org",
+            "@type": "BlogPosting",
             headline: post.metadata.title,
             datePublished: post.metadata.publishedAt,
             dateModified: post.metadata.publishedAt,
             description: post.metadata.summary,
-            image: post.metadata.image
-              ? `${baseUrl}${post.metadata.image}`
-              : `/og?title=${encodeURIComponent(post.metadata.title)}`,
-            url: `${baseUrl}/blog/${post.slug}`,
+            // image: post.metadata.image
+            //   ? `${baseUrl}${post.metadata.image}`
+            //   : `/og?title=${encodeURIComponent(post.metadata.title)}`,
+            // url: `${baseUrl}/blog/${post.slug}`,
             author: {
-              '@type': 'Person',
-              name: 'My Portfolio',
+              "@type": "Person",
+              name: "My Portfolio",
             },
           }),
         }}
@@ -94,5 +96,5 @@ export default function Blog({ params }) {
         <CustomMDX source={post.content} />
       </article>
     </section>
-  )
+  );
 }
