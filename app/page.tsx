e450d202f68f504c0d4848f840874536cd9dddc1
Ilham Ghifari <ilham@manyvisual.com>
2024-10-13 11:31:26 +0700

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { WritingPosts } from "@/components/posts";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { ArrowRight, Dot } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 function ReadMoreLink({ href }: { href: string }) {
