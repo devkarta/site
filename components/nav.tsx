d9400ab6bf1bf7aaf3083653e8f2678c339e8ba1
Ilham Ghifari <ilham@manyvisual.com>
2024-10-15 18:39:48 +0700

    make blog

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="-ml-[8px] mb-16 tracking-tight px-2">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-start justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-row space-x-4">
             {Object.entries(navConfig).map(([name, path]) => (
               <Link
                 key={name}
                 href={path}
-                className="transition-all hover:underline hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                className="transition-all hover:underline hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative  "
               >
                 {name}
               </Link>
